69a5ff7d71508042880997e06026f8f9a60b7a9a
wizard_blue <wizard@shrike-feather.com>
2018-12-02 10:17:33 +0900

    first commit

---
+++ twitch-notifier.js
@@ -0,0 +1,167 @@
+#!/usr/local/bin/node
+
+const TwitchWebhook = require('twitch-webhook')
+const twitter = require('twitter')
+const request = require('request')
+const fs = require('fs')
+const confFile = require('config')
+const log4js = require('log4js')
+
+log4js.configure({
+  appenders: { system: { type: 'dateFile', filename: 'logs/twitch-notifier.log', pattern: "-yyyyMMdd", compress: true } },
+  categories: { default: { appenders: ['system'], level: 'debug' } }
+})
+
+const logger = log4js.getLogger('system')
+
+const pidFile = confFile.config.pidFile
+
+if (!pidFile) {
+  logger.error('Pidfile is not provided')
+  process.exit(1)
+}
+
+const clientId = confFile.config.twitchClientId
+
+if (!clientId) {
+  logger.error('Twitch Client ID not provided')
+  process.exit(2)
+}
+
+const callBack = confFile.config.twitchCallBack
+
+if (!callBack) {
+  logger.error('Callback URL not provided')
+  process.exit(3)
+}
+
+const twitchUserId = confFile.config.twitchUserId
+
+if (!twitchUserId) {
+  logger.error('twitch user_id not provided')
+  process.exit(4)
+}
+
+const twitchSecret = confFile.config.twitchSecret
+
+if (!twitchSecret) {
+  logger.error('twitch secret for webhook api not provided')
+  process.exit(5)
+}
+
+const twitchChannel = confFile.config.twitchChannel
+
+if (!twitchChannel) {
+  logger.error('twitch channel URL not provided')
+  process.exit(6)
+}
+
+const twi_api = new twitter({
+	consumer_key: confFile.config.twitterConsumerKey,
+	consumer_secret: confFile.config.twitterConsumerSecret,
+	access_token_key: confFile.config.twitterAccessTokenKey,
+	access_token_secret: confFile.config.twitterAccessTokenSecret,
+})
+
+const twitchWebhook = new TwitchWebhook({
+  client_id: clientId,
+  callback: callBack,
+  secret: twitchSecret,
+  listen: {
+    port: 3000,
+    autoStart: true
+  }
+})
+
+fs.writeFile(pidFile, process.pid, (err) => {
+  if (err) {
+    logger.error(err)
+  }
+})
+
+// set listener for topic
+twitchWebhook.on('streams', ({ event }) => {
+    logger.debug(event)
+
+    if (event.data.length < 1) {
+	logger.error('empty data')
+	return
+    }
+
+    const queryURL = 'https://api.twitch.tv/helix/games?id=' + Number(event.data[0].game_id)
+
+    const headers = {
+	'Client-ID': clientId,
+    }
+
+    const options = {
+	url: queryURL,
+	method: 'GET',
+	headers: headers,
+	json: true
+    }
+
+    request(options, function (error, response, body) {
+	if (!error) {
+	    //success
+	    var content = 'Live: ' + body.data[0].name + '\n\n'
+	    content += event.data[0].title + '\n\n'
+	    content += twitchChannel
+
+	    twi_api.post('statuses/update',
+			{status: content},
+			function(error, tweet, response) {
+		if (error) {
+		    logger.error(error)
+		}
+	    })
+	} else {
+	    //sending request error
+	    logger.error(error)
+	}
+    })
+})
+
+twitchWebhook.subscribe('streams', {
+  user_id: twitchUserId
+})
+
+// renew the subscription when it expires
+twitchWebhook.on('unsubscibe', (obj) => {
+  logger.info('unsubscribe event caught. now re-subscribinng...')
+
+  twitchWebhook.subscribe('streams', {
+    user_id: twitchUserId
+  })
+})
+
+process.on('SIGINT', () => {
+  logger.info('SIGINT caught. unsubscribe then shutdown..')
+
+  twitchWebhook.unsubscribe('*')
+
+  process.exit(0)
+})
+
+process.on('SIGHUP', () => {
+
+  if (process.argv[2] != '-d') {
+    logger.info("SIGHUP caught, but i am not daemon. now shutting down myself..")
+
+    process.exit(0);
+  }
+
+  logger.info('SIGHUP caught. unsubscribe then re-subscribe..');
+
+  // unsubscribe from all topics
+  twitchWebhook.unsubscribe('*')
+
+  logger.info('unsubscribed. resubbing...')
+  
+  // subscribe again
+  twitchWebhook.subscribe('streams', {
+    user_id: twitchUserId
+  })
+
+  logger.info('re-subscribed.')
+})
