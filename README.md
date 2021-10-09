3d5ada53a248ab5ffd042801f7c357a4684c28f1
wizard_blue <wizard@shrike-feather.com>
2021-10-09 13:57:50 +0900

    twitch用ライブラリをtwurpleに変更、discordへの投稿をbotからに変更

--- README.md
+++ README.md
@@ -4,11 +4,6 @@
 ## 説明
 Twitchで配信を始めた時にtwitterに自動的にツイートするbotです。
 
-[![Image from Gyazo](https://i.gyazo.com/65d01ab3f039ea959f8a6e6be3f7f0b8.png)](https://gyazo.com/65d01ab3f039ea959f8a6e6be3f7f0b8)
-
-こんな感じにツイートされます。
-
-
 ## Usage
 ```
 twitch-notifier.js [-d]
@@ -18,42 +13,39 @@ twitch-notifier.js [-d]
     デーモン化して継続的に利用する際に指定してください。
 ```
 
-
 ## 動作確認済み環境・必要モジュール
 
 * 動作確認済み環境
 
-  FreeBSD 12.1-RELEASE上のnode v15.3.0にて動作を確認出来ています。
+  FreeBSD 12.2-RELEASE上のnode v16.10.0にて動作を確認出来ています。
 * 必要モジュール
 
-  twitch-webhook, twitter, request, fs, config, log4js
+  @twurple/api, @twurple/auth, @twurple/eventsub, @twurple/eventsub-ngrok, twitter, discord.js, fs, config, log4js
 
 
 ## 使用手順
 1. 上記必要モジュールをインストールします。
 
-2. twitch APIのOAuthトークンを以下のようにcurlで取得します。  
-OAuthトークンについての詳細は以下を参照してください。  
-https://dev.twitch.tv/docs/authentication/  
-scopeについては以下を参照してください。  
-https://dev.twitch.tv/docs/authentication/#scopes
+2. twitchからのwebhookのコールバック用のゲートウェイとしてngrokを使うので、  
+ngrokの初期設定を済ませます。  
+https://dashboard.ngrok.com/get-started/setup
 
-```bash
-curl -k 'https://id.twitch.tv/oauth2/token? \
-client_id=<Client-ID>& \
-client_secret=<ClientSecret>& \
-grant_type=client_credentials& \
-scope=user:read:broadcast+analytics:read:games' \
--X POST
-```
+3. discordのbotのトークンを取得します。  
+トークンの取得方法は以下を参考にしてください：  
+https://qiita.com/1ntegrale9/items/cb285053f2fa5d0cccdf#%E3%82%A2%E3%82%AF%E3%82%BB%E3%82%B9%E3%83%88%E3%83%BC%E3%82%AF%E3%83%B3%E3%81%AE%E5%8F%96%E5%BE%97
 
-3. discord通知用にwebhookを作成します。  
-webhookの詳細・作成方法については以下を参照してください。
-https://support.discord.com/hc/ja/articles/228383668-%E3%82%BF%E3%82%A4%E3%83%88%E3%83%AB-Webhooks%E3%81%B8%E3%81%AE%E5%BA%8F%E7%AB%A0
+4. discordのbotを設定し、botを目的のサーバーに追加します。  
+bot用の権限は"Manage Roles", "Send Messages", "Manage Messages", "Manage Threads"を選びます。  
+botの権限設定については以下を参考にしてください：  
+https://qiita.com/1ntegrale9/items/cb285053f2fa5d0cccdf#%E8%AA%8D%E8%A8%BCurl%E3%81%A7%E6%A8%A9%E9%99%90%E3%82%92%E8%A8%AD%E5%AE%9A%E3%81%99%E3%82%8B
 
-4. discord通知用のRoleをサーバーの中に作成します。 
+5. discord通知用のRoleをサーバーの中に作成します。 
 
-5. discord通知用Role IDを取得します。  
+6. discord通知用チャンネルIDを取得します。  
+取得方法に関しては以下の記事を参考にしてください：  
+https://support.discord.com/hc/ja/articles/206346498-%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC-%E3%82%B5%E3%83%BC%E3%83%90%E3%83%BC-%E3%83%A1%E3%83%83%E3%82%BB%E3%83%BC%E3%82%B8ID%E3%81%AF%E3%81%A9%E3%81%93%E3%81%A7%E8%A6%8B%E3%81%A4%E3%81%91%E3%82%89%E3%82%8C%E3%82%8B-
+
+7. discord通知用Role IDを取得します。  
 Roleを追加したサーバー内の任意のテキストチャンネル内で  
 以下のように入力するとRole IDを取得することが出来ます。
 
@@ -61,24 +53,20 @@ Roleを追加したサーバー内の任意のテキストチャンネル内で
 \@Role名
 ```
 
-6. 設定ファイル(config/default.json)に以下の記述を書き入れます。
+8. 設定ファイル(config/default.json)に以下の記述を書き入れます。
 
 ---
 <dl>
 <dt>pidFile</dt>
   <dd>pidファイルのファイルパスを設定します。-dオプションを付けて起動している時に、HUPシグナルを受け取った際にここに記述されるプロセスIDに対してHUPシグナルを送ります。</dd>
-<dt>twitchUserId</dt>
-  <dd>twitch APIで取得出来るユーザーID（数値）を設定します。</dd>
-<dt>twitchCallBack</dt>
-  <dd>twitchに通知するコールバックURLを設定します。</dd>
+<dt>twitchUserName</dt>
+  <dd>通知対象のtwitchユーザー名を設定します。</dd>
 <dt>twitchClientId</dt>
   <dd>twitch APIのクライアントIDを設定します。</dd>
 <dt>twitchSecret</dt>
-  <dd>twitch webhook APIで使用するシークレットを設定します。任意の文字列を使ってください。</dd>
-<dt>twitchOAuthtoken</dt>
-  <dd>twitch APIのOAuthトークンを設定します。</dd>
+  <dd>twitch APIのシークレットを設定します。</dd>
 <dt>twitchChannel</dt>
-  <dd>実際に通知したいtwitchのチャンネル名を設定します。ここに書いたURLがツイートに載ります。</dd>
+  <dd>実際に通知したいtwitchのチャンネルURLを設定します。ここに書いたURLがツイートに載ります。</dd>
 <dt>twitterConsumerKey</dt>
   <dd>twitter APIのコンシューマキーを設定します。</dd>
 <dt>twitterConsumerSecret</dt>
@@ -87,15 +75,15 @@ Roleを追加したサーバー内の任意のテキストチャンネル内で
   <dd>twitter APIのアクセストークンキーを設定します。</dd>
 <dt>twitterAccessTokenSecret</dt>
   <dd>twitter APIのアクセストークンシークレットを設定します。</dd>
-<dt>discordWebhookURL</dt>
-  <dd>discordへの通知のためのwebhook URLを設定します。</dd>
+<dt>discordToken</dt>
+  <dd>discord botのトークンを設定します。</dd>
 <dt>discordRoleId</dt>
   <dd>discordで通知する宛先のRole IDを設定します。</dd>
 </dl>
 
 ---
 
-7. スクリプトを起動します。例ではバックグラウンドに落としていますが、デーモン化したい場合は適宜デーモン化してください。
+9. スクリプトを起動します。例ではバックグラウンドに落としていますが、デーモン化したい場合は適宜デーモン化してください。
 ex)
 ```bash
 (./twitch-notifier.js -d) &
--- twitch-notifier.js
+++ twitch-notifier.js
@@ -1,214 +1,177 @@
 #!/usr/local/bin/node
 
-const TwitchWebhook = require('twitch-webhook')
-const twitter = require('twitter')
-const request = require('request')
-const fs = require('fs')
-const confFile = require('config')
-const log4js = require('log4js')
+const confFile = require('config');
+const log4js = require('log4js');
+const fs = require('fs');
+const { ClientCredentialsAuthProvider } = require('@twurple/auth');
+const { ApiClient } = require('@twurple/api');
+const {
+	DirectConnectionAdapter,
+	EventSubChannelUpdateEvent,
+	EventSubListener,
+} = require('@twurple/eventsub');
+const { NgrokAdapter } = require('@twurple/eventsub-ngrok');
+const twitter = require('twitter');
+const Discord = require('discord.js');
 
 log4js.configure({
   appenders: { system: { type: 'dateFile', filename: 'logs/twitch-notifier.log', pattern: "-yyyyMMdd", compress: true } },
   categories: { default: { appenders: ['system'], level: 'debug' } }
-})
+});
 
-const logger = log4js.getLogger('system')
+const logger = log4js.getLogger('system');
 
-const pidFile = confFile.config.pidFile
-
-if (!pidFile) {
-  logger.error('Pidfile is not provided')
-  process.exit(1)
+if (!confFile.config.pidFile) {
+  logger.error('Pidfile is not provided');
+  process.exit(1);
 }
 
-const clientId = confFile.config.twitchClientId
-
-if (!clientId) {
-  logger.error('Twitch Client ID not provided')
-  process.exit(2)
+if (!confFile.config.twitchClientId) {
+  logger.error('Twitch Client ID not provided');
+  process.exit(2);
 }
 
-const callBack = confFile.config.twitchCallBack
-
-if (!callBack) {
-  logger.error('Callback URL not provided')
-  process.exit(3)
+if (!confFile.config.twitchUserName) {
+  logger.error('twitch username not provided');
+  process.exit(4);
 }
 
-const twitchUserId = confFile.config.twitchUserId
-
-if (!twitchUserId) {
-  logger.error('twitch user_id not provided')
-  process.exit(4)
+if (!confFile.config.twitchSecret) {
+  logger.error('twitch secret for webhook api not provided');
+  process.exit(5);
 }
 
-const twitchSecret = confFile.config.twitchSecret
-
-if (!twitchSecret) {
-  logger.error('twitch secret for webhook api not provided')
-  process.exit(5)
+if (!confFile.config.twitchChannel) {
+  logger.error('twitch channel URL not provided');
+  process.exit(6);
 }
 
-const twitchOAuthtoken = confFile.config.twitchOAuthtoken
-
-if (!twitchOAuthtoken) {
-  logger.error('twitch OAuth token for webhook api not provided')
-  process.exit(5)
+if (!confFile.config.discordChannel) {
+  logger.error('discord channel not provided');
+  process.exit(7);
 }
 
-const twitchChannel = confFile.config.twitchChannel
-
-if (!twitchChannel) {
-  logger.error('twitch channel URL not provided')
-  process.exit(6)
+if (!confFile.config.discordToken) {
+  logger.error('discord token not provided');
+  process.exit(7);
 }
 
-const discordWebhookURL = confFile.config.discordWebhookURL
-
-if (!discordWebhookURL) {
-  logger.error('discord webhook URL not provided')
-  process.exit(7)
+if (!confFile.config.discordRoleId) {
+  logger.error('discord roll id is not provided');
+  process.exit(7);
 }
 
-const discordRoleId = confFile.config.discordRoleId
-
-if (!discordRoleId) {
-  logger.error('discord roll id is not provided')
-  process.exit(7)
-}
+const DiscordClient = new Discord.Client({
+  intents: Discord.Intents.FLAGS.GUILDS | Discord.Intents.FLAGS.GUILD_MESSAGES
+});
 
 const twi_api = new twitter({
-	consumer_key: confFile.config.twitterConsumerKey,
-	consumer_secret: confFile.config.twitterConsumerSecret,
-	access_token_key: confFile.config.twitterAccessTokenKey,
-	access_token_secret: confFile.config.twitterAccessTokenSecret,
-})
+  consumer_key: confFile.config.twitterConsumerKey,
+  consumer_secret: confFile.config.twitterConsumerSecret,
+  access_token_key: confFile.config.twitterAccessTokenKey,
+  access_token_secret: confFile.config.twitterAccessTokenSecret,
+});
 
-const twitchWebhook = new TwitchWebhook({
-  client_id: clientId,
-  callback: callBack,
-  secret: twitchSecret,
-  oauth_token: twitchOAuthtoken,
-  listen: {
-    port: 3000,
-    autoStart: true
-  }
-})
-
-fs.writeFile(pidFile, process.pid.toString(), (err) => {
+fs.writeFile(confFile.config.pidFile, process.pid.toString(), (err) => {
   if (err) {
-    logger.error(err)
+    logger.error(err);
   }
-})
+});
+
+const authProvider = new ClientCredentialsAuthProvider(confFile.config.twitchClientId, confFile.config.twitchSecret);
+const apiClient = new ApiClient({ authProvider: authProvider });
+const secret = Math.random().toString(32).substring(2);
+
+const listener = new EventSubListener({
+	apiClient,
+	adapter: new NgrokAdapter(),
+	secret
+});
 
 function twitterpost(game_name, streaming_title) {
-    var content = 'Live: ' + game_name + '\n\n'
-    content += streaming_title + '\n\n'
-    content += twitchChannel
+    var content = 'Live: ' + game_name + '\n\n';
+    content += streaming_title + '\n\n';
+    content += confFile.config.twitchChannel;
 
     twi_api.post('statuses/update',
 		 {status: content},
 		 function(error, tweet, response) {
-	if (error) {
-	    logger.error(error)
-	}
-    })
+      if (error) {
+        logger.error(error);
+      }
+    });
 
-    const discordwhoptions = {
-	url: discordWebhookURL,
-	method: 'POST',
-	headers: {
-		"Content-type": "application/json",
-	      },
-	json: {
-		"username": "twitchnotifier",
-		"content": "<@&" + discordRoleId + "> " + content,
-	}
-    }
+    DiscordClient.channels.cache.get(confFile.config.discordChannel).send(
+	"<@&" + confFile.config.discordRoleId + "> " + content
+    );
+}
 
-    request(discordwhoptions, function (error, response, body) {
-	if (!error) {
-	    //success
-	    logger.info('posting infomation to discord webhook succeeded')
-	} else {
-	    //sending request error
-	    logger.error(error)
-	}
-    })
+async function init(){
+  try {
+    await apiClient.eventSub.deleteAllSubscriptions();
+    await listener.listen();
+    await DiscordClient.login(confFile.config.discordToken);
+  } catch(err) {
+    logger.error(err);
+  }
 }
 
 // set listener for streaming topic
-twitchWebhook.on('streams', ({ event }) => {
-    logger.debug(event)
+async function twitchsubscribe(){
+  try {
+    const user = await apiClient.users.getUserByName(confFile.config.twitchUserName);
 
-    if (event.data.length < 1) {
-	logger.error('empty data')
-	return
-    }
+    await listener.subscribeToStreamOnlineEvents(user.id, event => {
+	let status;
 
-    const queryURL = 'https://api.twitch.tv/helix/games?id=' + Number(event.data[0].game_id)
-
-    const headers = {
-	'Client-ID': clientId,
-	'Authorization': 'Bearer ' + twitchOAuthtoken,
-    }
-
-    const options = {
-	url: queryURL,
-	method: 'GET',
-	headers: headers,
-	json: true
-    }
-
-    if (event.data[0].game_id == '0' || event.data[0].game_id == '') {
-	twitterpost('', event.data[0].title)
-    } else {
-	request(options, function (error, response, body) {
-	    if (!error) {
-		//success
-		twitterpost(body.data[0].name, event.data[0].title)
-	    } else {
-		//sending request error
-		logger.error(error)
-	    }
-	})
-    }
-})
-
-
-// subscribe 
-twitchWebhook.subscribe('streams', {
-  user_id: twitchUserId
-})
+	(async function(){
+	    status = await event.getStream();
+	    twitterpost(status.gameName, status.title);
+	})();
+    });
+  } catch(err) {
+    logger.error(err);
+  }
+}
 
 // process event handlers
 process.on('SIGINT', () => {
-  logger.info('SIGINT caught. unsubscribe then shutdown..')
+  logger.info('SIGINT caught. unsubscribe then shutdown..');
 
-  twitchWebhook.unsubscribe('*')
+  (async function(){
+    await apiClient.eventSub.deleteAllSubscriptions();
+  })();
 
-  process.exit(0)
+  process.exit(0);
 })
 
 process.on('SIGHUP', () => {
 
   if (process.argv[2] != '-d') {
-    logger.info("SIGHUP caught, but i am not daemon. now shutting down myself..")
+    logger.info("SIGHUP caught, but i am not daemon. now shutting down myself..");
 
     process.exit(0);
   }
 
-  logger.info('SIGHUP caught. unsubscribe then re-subscribe..');
+  logger.info('SIGHUP caught. unlisten/unsubscribe then re-subscribe..');
 
   // unsubscribe from all topics
-  twitchWebhook.unsubscribe('*')
+  (async function(){
+    await apiClient.eventSub.deleteAllSubscriptions();
+    await listener.unlisten();
+  })();
 
-  logger.info('unsubscribed. resubbing...')
+  logger.info('unlistened. resubbing...');
   
   // subscribe again
-  twitchWebhook.subscribe('streams', {
-    user_id: twitchUserId
-  })
+  (async function(){
+    await listener.listen();
+  })();
 
-  logger.info('re-subscribed.')
+  twitchsubscribe();
+
+  logger.info('re-subscribed.');
 })
+
+init();
+twitchsubscribe();
